1aa7a6237a7e360b0ae24b828dc7dee4391d4ce0
Scabandari <ryan3nichols@gmail.com>
2018-10-02 19:51:53 -0400

    client can send msg's over UDP and TCP

---
+++ client.py
@@ -0,0 +1,68 @@
+import socket
+import ast
+from utils import get_user_command
+import threading
+
+"""Here I'm thinking we need 2 threads in addition to the main thread. One that constantly checks 
+    a list to see if there are user msg's to be send over UDP and then same for TCP. When user 
+    decides from main thread to answer at terminal they want to register they'll give some info and 
+    we create a msg and put into the list the UDP thread keeps checking"""
+udp_messages = []
+udp_msg_lock = threading.Lock()
+tcp_messages = []
+tcp_msg_lock = threading.Lock()
+terminal_lock = threading.Lock()
+CHOICES = ['r']
+HOST = "192.168.0.106"  # this would normally be different and particular to the host machine ie client
+PORT = 5001  # UDP port
+# todo check w/ prof that the server ip address will be known and don't need to get it programmatically
+SERVER = ("192.168.0.106", 5023)
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # todo need this? test pls
+udp_socket.bind((HOST, PORT))
+
+tcp_ip = HOST  # won't usually, get tcp servers machines local ip address on LAN
+tcp_port = 5006
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_socket.connect((tcp_ip, tcp_port))
+
+
+def tcp_function():
+    while True:
+        if tcp_messages:
+            with tcp_msg_lock:
+                msg = tcp_messages.pop(0)
+            tcp_socket.send(msg)
+            return_msg = tcp_socket.recv(1024).decode('ascii')
+            print("Received back tcp response: " + return_msg)
+
+
+# todo rename this
+def udp_function():
+    while True:
+        if udp_messages:
+            with udp_msg_lock:
+                msg = udp_messages.pop(0)
+            udp_socket.sendto(msg, SERVER)
+            response, addr = udp_socket.recvfrom(1024)
+            response = response.decode('utf-8')
+            print(response)
+
+
+udp_thread = threading.Thread(target=udp_function)
+udp_thread.start()
+
+tcp_thread = threading.Thread(target=tcp_function)
+tcp_thread.start()
+
+while True:
+    get_user_command(
+        udp_messages,
+        udp_msg_lock,
+        tcp_messages,
+        tcp_msg_lock
+    )
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from random import randint
 import json
-CHOICES = ['r']
+CHOICES = ['r', 'b']
 REGISTER = 'REGISTER'
 REGISTERED = 'REGISTERED'
 
@@ -8,21 +8,30 @@ REGISTERED = 'REGISTERED'
 # https://www.tutorialspoint.com/python3/python_files_io.htm
 
 
-def send_udp(msg, socket_, server):  # todo needs socket param?
-    msg = str.encode(msg)
-    socket_.sendto(msg, server)
-    response, addr = socket_.recvfrom(1024)
-    response = response.decode('utf-8')
-    print(response)
+# def send_udp_msg(msg, socket_, server):  # todo needs socket param?
+#     msg = str.encode(msg)
+#     socket_.sendto(msg, server)
+#     response, addr = socket_.recvfrom(1024)
+#     response = response.decode('utf-8')
+#     print(response)
+#
+#
+# def send_tcp_msg():
+#     pass
 
 
 # todo this possibly returns None, maybe change this
-def get_user_command(state=None ):
+def get_user_command(udp_queue, udp_queue_lock, tcp_queue, tcp_queue_lock):
     """This function gives the user their options of different actions they can take
         and either returns None if their choice doesn't exist or the msg to be send
          over UDP to the server"""
-    choice = input("Enter 'r' if you would like to register: ")
+    choice = input("Enter a command: \n" +
+                   "'r' ==> Registration and Registration\n" +
+                   "'b' ==> Bidding, putting items up for bid etc.\n" +
+                   ":: ")
     if choice is CHOICES[0]:
+        # todo make function to give user their registration options and send UDP msg
+        # todo same for TCP
         name = input("Enter your unique name identifier: ")
         ip_address = input("Enter the ip address of the computer you'll be bidding from: ")
         port = input("Enter the port number your computer will be listening on: ")
@@ -33,10 +42,34 @@ def get_user_command(state=None ):
                     'ip': ip_address,
                     'port': port}
         send_bytes = dict_to_bytes(send_msg)
+        with udp_queue_lock:
+            udp_queue.append(send_bytes)
+
+    elif choice is CHOICES[1]:
+        selection = input("Enter a command: \n" +
+                          "'of' ==> Offer an item for auction\n::")
+        if selection == 'of':
+            # name = input("Enter your unique name identifier: ")
+            # ip_address = input("Enter the ip address of your computer: ")
+            # description = input("Enter a description of the item on offer: ")
+            # min_bid = input("Enter the minimum bid: ")
+            # request_number = randint(0, 100000)
+            #
+            # send_msg = {'type': 'OFFER',
+            #             'request': request_number,
+            #             'name': name,
+            #             'ip': ip_address,
+            #             'description': description,
+            #             'minimum bid': min_bid}
+            #send_bytes = dict_to_bytes(send_msg)
+            test_msg = "Successfully sent over tcp"
+            with tcp_queue_lock:
+                #tcp_queue.append(send_bytes)
+                tcp_queue.append(test_msg.encode('ascii'))
+
     else:
         print("That option isn't available")
         return None
-    return send_bytes
 
 
 def dict_to_bytes(dict_):
