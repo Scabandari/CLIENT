5e6ac5ba4d81e7b40b3f1eff3476480ef5748d9d
Scabandari <ryan3nichols@gmail.com>
2018-10-16 19:05:57 -0400

    Client can now send an offer on an item if registerd

--- client.py
+++ client.py
@@ -12,29 +12,37 @@ udp_messages = []
 udp_msg_lock = threading.Lock()
 tcp_messages = []
 tcp_msg_lock = threading.Lock()
+
+tcp_messages_returned = []
+tcp_ret_lock = threading.Lock()
 terminal_lock = threading.Lock()
 CHOICES = ['r']
 HOST = "192.168.0.107"  # this would normally be different and particular to the host machine ie client
 PORT = 5075  # UDP port
 # todo check w/ prof that the server ip address will be known and don't need to get it programmatically
-SERVER = ("192.168.0.107", 5023)
+SERVER = ("192.168.0.107", 5024)
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # todo need this? test pls
 udp_socket.bind((HOST, PORT))
 
 tcp_ip = HOST  # won't usually, get tcp servers machines local ip address on LAN
-tcp_port = 5001
+tcp_port = 5002
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.connect((tcp_ip, tcp_port))
 
 
 def tcp_function():
     while True:
+        # if tcp_messages_returned:
+        #     with tcp_ret_lock:
+        #         tcp_messages_returned.pop(0)
+        #     print("Received back t")
         if tcp_messages:
             with tcp_msg_lock:
                 msg = tcp_messages.pop(0)
             tcp_socket.send(msg)
-            return_msg = tcp_socket.recv(1024).decode('ascii')
+            #return_msg = tcp_socket.recv(1024).decode('ascii')
+            return_msg = tcp_socket.recv(1024).decode('utf-8')
             print("Received back tcp response: " + return_msg)
 
 
@@ -61,7 +69,9 @@ while True:
         udp_messages,
         udp_msg_lock,
         tcp_messages,
-        tcp_msg_lock
+        tcp_msg_lock,
+        tcp_messages_returned,
+        tcp_ret_lock
     )
 
 
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from random import randint
 import json
-CHOICES = ['r', 'b']
+#CHOICES = ['r', 'b']
 REGISTER = 'REGISTER'
 REGISTERED = 'REGISTERED'
 
@@ -21,18 +21,31 @@ REGISTERED = 'REGISTERED'
 
 
 # todo this possibly returns None, maybe change this
-def get_user_command(udp_queue, udp_queue_lock, tcp_queue, tcp_queue_lock):
+def get_user_command(
+        udp_queue,
+        udp_queue_lock,
+        tcp_queue,
+        tcp_queue_lock,
+        tcp_msg_return,
+        tcp_ret_lock):
     """This function gives the user their options of different actions they can take
         and either returns None if their choice doesn't exist or the msg to be send
          over UDP to the server"""
+    with tcp_ret_lock:
+        if tcp_msg_return:
+            return_msg = tcp_msg_return.pop(0)
+            print("Return msg: {}".format(return_msg))
     choice = input("Enter a command: \n" +
                    "'r' ==> Registration and Registration\n" +
                    "'b' ==> Bidding, putting items up for bid etc.\n" +
-                   ":: ")
-    if choice is CHOICES[0]:
-        name = input("Enter your unique name identifier: ")
-        ip_address = input("Enter the ip address of the computer you'll be bidding from: ")
-        port = input("Enter the port number your computer will be listening on: ")
+                   "'c' ==> Cancel, will free up terminal to display any responses from server::")
+    if choice is 'r':
+        # name = input("Enter your unique name identifier: ")
+        # ip_address = input("Enter the ip address of the computer you'll be bidding from: ")
+        # port = input("Enter the port number your computer will be listening on: ")
+        name = 'ryan'
+        ip_address = '192.168.0.107'
+        port = 5000
         request_number = randint(0, 100000)
         send_msg = {'request': request_number,
                     'type': REGISTER,
@@ -43,14 +56,19 @@ def get_user_command(udp_queue, udp_queue_lock, tcp_queue, tcp_queue_lock):
         with udp_queue_lock:
             udp_queue.append(send_bytes)
 
-    elif choice is CHOICES[1]:
+    elif choice is 'b':
         selection = input("Enter a command: \n" +
-                          "'of' ==> Offer an item for auction\n::")
+                          "'of' ==> Offer an item for auction\n" +
+                          "'b' ==> Bid on an item\n::")
         if selection == 'of':
-            name = input("Enter your unique name identifier: ")
-            ip_address = input("Enter the ip address of your computer: ")
-            description = input("Enter a description of the item on offer: ")
-            min_bid = input("Enter the minimum bid: ")
+            # name = input("Enter your unique name identifier: ")
+            # ip_address = input("Enter the ip address of your computer: ")
+            # description = input("Enter a description of the item on offer: ")
+            # min_bid = input("Enter the minimum bid: ")
+            name = 'ryan'
+            ip_address = '192.168.0.107'
+            description = 'bat'
+            min_bid = 10
             request_number = randint(0, 100000)
 
             send_msg = {'type': 'OFFER',
@@ -60,10 +78,12 @@ def get_user_command(udp_queue, udp_queue_lock, tcp_queue, tcp_queue_lock):
                         'description': description,
                         'minimum bid': min_bid}
             send_bytes = dict_to_bytes(send_msg)
-            test_msg = "Successfully sent over tcp"
+            #test_msg = "Successfully sent over tcp"
             with tcp_queue_lock:
                 tcp_queue.append(send_bytes)
                 #tcp_queue.append(test_msg.encode('utf-8'))
+    elif choice is 'c':
+        return
     else:
         print("That option isn't available")
         return None
